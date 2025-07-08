ba4660133c9e09fe33731204a0c38aeedec8bd26
DevInside <kecondev@gmail.com>
2025-07-08 12:14:54 +0900

    20250708 함수 첫번째

--- chapter4/4-1-함수.py
+++ chapter4/4-1-함수.py
@@ -18,3 +18,23 @@ print(subtract(5, 3))   # 2
 print(multiply(5, 3))   # 15
 print(divide(5, 3))     # 1.6666666666666667
 print(divide(5, 0))     # Division by zero error
+
+
+def add_multi(choice, *args):
+    if choice == "add":
+        return sum(args)
+    elif choice == "multi":
+        result = 1
+        for num in args:
+            result *= num
+        return result
+    else:
+        return "Invalid choice"
+    
+print(add_multi("add", 1, 2, 3, 4))
+print(add_multi("multi", 1, 2, 3, 4))
+print(add_multi("other", 1, 2, 3, 4))   
+
+
+lambda_add = lambda x, y: x + y
+print(lambda_add(5, 3))  # 8
